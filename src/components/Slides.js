df0c4f27a20214c4f57a3b67eabf6d32601845b1
Piyush Nanwani <pnanwani61@gmail.com>
2021-04-17 10:56:03 +0530

    added code for slideshow

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Slides({slides}) {
+    const [slideIdx, setSlideIdx] = useState(0);
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={()=>setSlideIdx(0)} disabled={slideIdx===0? true: false}>Restart</button>
+                <button data-testid="button-prev" className="small" onClick={()=>setSlideIdx(slideIdx-1)} disabled={slideIdx===0? true: false} >Prev</button>
+                <button data-testid="button-next" className="small" onClick={()=>setSlideIdx(slideIdx+1)} disabled={slideIdx===slides.length-1? true: false}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[slideIdx].title}</h1>
+                <p data-testid="text">{slides[slideIdx].text}</p>
             </div>
         </div>
     );
